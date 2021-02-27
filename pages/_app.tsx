e7e84505382c43741dd17ae21e68eb138fc05415
Timothy Clark <TheCoolBlackCat@users.noreply.github.com>
2021-02-27 22:19:58 +0000

    Set Title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,17 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import "@fortawesome/fontawesome-free/css/all.min.css"
 import "../styles/globals.css"
+import Head from "next/head"
 
 function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (<div>
+      <Head>
+        <title>CryptoTracker</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default App
